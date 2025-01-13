1a22913023858e0fd3c5c57a71dc873b00183e8b
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2025-01-13 14:17:54 +0000

    Refactor Loader and adjust UploadBox layout.
    Moved Loader inside UploadBox for better alignment and visibility. Enhanced Loader with improved styling and a more descriptive loading message. Refactored UploadBox's structure to integrate Loader dynamically based on submission state.

--- frontend/src/components/UploadBox.tsx
+++ frontend/src/components/UploadBox.tsx
@@ -16,11 +16,15 @@ function UploadBox(props: {allowSubmission: boolean,  onSubmit: (event: never) =
                     id="fileInput"
                     onChange={props.onSubmit}
                     disabled={!props.allowSubmission}
-                    className="mt-2 block w-full mx-auto text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
+                    className="mt-2  mx-auto text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
                 />
             </form>
+            <div className="flex justify-center mx-auto">
+
+                {!props.allowSubmission ? <Loader/> : <></>}
+
+            </div>
         </div>
-        {!props.allowSubmission ? <Loader/> : <></>}
     </>;
 }
 
