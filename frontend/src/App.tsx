ecb5f07b594acbfdb8e7672e0c12191286cfca9b
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2025-01-14 14:06:17 +0000

    Remove unused loadModelFromOPFS function
    The loadModelFromOPFS function was deleted as it was no longer in use. This helps simplify the codebase and eliminate unnecessary complexity or dead code. No changes were made to the existing functionality.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,33 +19,6 @@ function saveMap(fileData: Uint8Array, outputName: string) {
   document.body.removeChild(link);
 }
 
-const loadModelFromOPFS = async (modelName: string, modelUrl: string) => {
-  try {
-    const root = await navigator.storage.getDirectory();
-    console.log("Getting", modelName, "from OPFS.")
-    return root.getFileHandle(modelName).then(async (fileHandle) => {
-      console.log("Found", modelName, "in OPFS.")
-      const file = await fileHandle.getFile();
-      return await file.arrayBuffer();
-    }).catch(async () => {
-      const response = await fetch(modelUrl);
-      if (!response.ok) {
-        console.error("Failed to fetch model from remote URL.");
-      }
-      const modelBlob = await response.blob();
-
-      const newFileHandle = await root.getFileHandle(modelName, {create: true});
-      const writable = await newFileHandle.createWritable();
-      await writable.write(modelBlob);
-      await writable.close();
-
-      return modelBlob.arrayBuffer();
-    });
-
-  } catch (error: unknown) {
-    console.error("Error loading model from OPFS.", error);
-  }
-
 
 function App() {
   const [fileContent, setFileContent] = useState<null | Uint8Array>(null);
