c77456b32af746abd4bd1573a9e11ae1d0f010c2
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2025-01-14 11:19:03 +0000

    Refactor model loading and disable StrictMode in rendering.
    Re-enable OPFS-based model loading with improved error handling and update ONNX session creation to use loaded model blobs. Also, temporarily disable React StrictMode in the app's rendering to avoid duplicate effects during development.

--- frontend/src/workers/inferenceWorker.ts
+++ frontend/src/workers/inferenceWorker.ts
@@ -4,49 +4,45 @@
 
 import * as ort from "onnxruntime-web";
 ort.env.wasm.wasmPaths = "/";
-ort.env.wasm.numThreads = 16;
+ort.env.wasm.numThreads = 8;
 
 
-// const loadModelFromOPFS = async (modelName: string, modelUrl: string) => {
-//   try {
-//     const root = await navigator.storage.getDirectory();
-//     console.log("Getting", modelName, "from OPFS.")
-//     return root.getFileHandle(modelName).then(async (fileHandle) => {
-//       console.log("Found", modelName, "in OPFS.")
-//       const file = await fileHandle.getFile();
-//       return await file.arrayBuffer();
-//     }).catch(async () => {
-//       const response = await fetch(modelUrl);
-//       if (!response.ok) {
-//         console.error("Failed to fetch model from remote URL.");
-//       }
-//       const modelBlob = await response.blob();
-//
-//       const newFileHandle = await root.getFileHandle(modelName, {create: true});
-//       const writable = await newFileHandle.createWritable();
-//       await writable.write(modelBlob);
-//       await writable.close();
-//
-//       return modelBlob.arrayBuffer();
-//     });
-//
-//   } catch (error: unknown) {
-//     console.error("Error loading model from OPFS.", error);
-//   }
-// }
+const loadModelFromOPFS = async (modelName: string, modelUrl: string) => {
+  try {
+    const root = await navigator.storage.getDirectory();
+    console.log("Getting", modelName, "from OPFS.")
+    return root.getFileHandle(modelName).then(async (fileHandle) => {
+      console.log("Found", modelName, "in OPFS.")
+      const file = await fileHandle.getFile();
+      return await file.arrayBuffer();
+    }).catch(async () => {
+      const response = await fetch(modelUrl);
+      if (!response.ok) {
+        console.error("Failed to fetch model from remote URL.");
+      }
+      const modelBlob = await response.blob();
+
+      const newFileHandle = await root.getFileHandle(modelName, {create: true});
+      const writable = await newFileHandle.createWritable();
+      await writable.write(modelBlob);
+      await writable.close();
+
+      return modelBlob.arrayBuffer();
+    });
+
+  } catch (error: unknown) {
+    console.error("Error loading model from OPFS.", error);
+  }
+}
 
 const loadModel = async (modelName: string, modelUrl: string) => {
   try {
-    // const modelBlob = await loadModelFromOPFS(modelName, modelUrl);
-    // if (!modelBlob) {
-    //   console.error("Failed to load model blob from OPFS and/or remote.")
-    //   return
-    // } else {
-    //   console.log("Loaded model blob from OPFS and/or remote.", modelBlob.byteLength, "bytes.")
-    // }
-    // console.log("Creating ONNX model session...")
+    const modelBlob = await loadModelFromOPFS(modelName, modelUrl);
+    if (!modelBlob) {console.error("Failed to load model blob from OPFS and/or remote."); return;}
+    else {console.log("Loaded model blob from OPFS and/or remote.", modelBlob.byteLength, "bytes.")}
+
     console.log(modelName)
-    const session = await ort.InferenceSession.create(modelUrl);
+    const session = await ort.InferenceSession.create(modelBlob);
     console.log("ONNX model loaded successfully.");
     return session;
   } catch (err) {
